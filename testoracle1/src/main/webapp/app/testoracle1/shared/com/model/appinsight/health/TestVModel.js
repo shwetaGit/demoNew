9e895a5446238145547ba0acbe271d2784cfaa09
shweta.zagade@algorhythm.co.in <applifire@bfc453c60a3b>
2016-05-11 14:00:26 +0000

    d

---
+++ testoracle1/src/main/webapp/app/testoracle1/shared/com/model/appinsight/health/TestVModel.js
@@ -0,0 +1,31 @@
+Ext.define('Testoracle1.testoracle1.shared.com.model.appinsight.health.TestVModel', {
+     "extend": "Ext.data.Model",
+     "fields": [{
+          "name": "primaryKey",
+          "type": "auto",
+          "defaultValue": ""
+     }, {
+          "name": "ttid",
+          "type": "string",
+          "defaultValue": ""
+     }, {
+          "name": "tmn",
+          "type": "string",
+          "defaultValue": ""
+     }, {
+          "name": "tno",
+          "type": "string",
+          "defaultValue": ""
+     }, {
+          "name": "versionId",
+          "type": "int",
+          "defaultValue": ""
+     }, {
+          "name": "entityAudit",
+          "reference": "EntityAudit"
+     }, {
+          "name": "primaryDisplay",
+          "type": "string",
+          "defaultValue": ""
+     }]
+});
