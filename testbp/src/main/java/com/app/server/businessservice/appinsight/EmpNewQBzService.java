fac1865d6e65228e654f3d7dadfa46481eb2c43e
shweta.zagade@algorhythm.co.in <applifire@836552917ff5>
2016-06-03 13:09:07 +0000

    File deleting

--- testbp/src/main/java/com/app/server/businessservice/appinsight/EmpNewQBzService.java
+++
@@ -1,8 +0,0 @@
-package com.app.server.businessservice.appinsight;
-import com.app.shared.appinsight.EmpNewQ;
-import java.util.List;
-
-public interface EmpNewQBzService {
-
-    public List<EmpNewQ> executeQueryEmpNewQ() throws Exception;
-}
