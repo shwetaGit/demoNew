fb590c977e9b7435db5d43c72e0a5a0a8093caca
shweta.zagade@algorhythm.co.in <applifire@836552917ff5>
2016-06-03 12:49:02 +0000

    File deleting

--- testbp/src/main/webapp/app/testbp/shared/com/viewmodel/testbc/testingdomain/StudViewModel.js
+++
@@ -1,6 +0,0 @@
-Ext.define('Testbp.testbp.shared.com.viewmodel.testbc.testingdomain.StudViewModel', {
-     "extend": "Ext.app.ViewModel",
-     "alias": "viewmodel.StudViewModel",
-     "model": "StudModel",
-     "data": {}
-});
