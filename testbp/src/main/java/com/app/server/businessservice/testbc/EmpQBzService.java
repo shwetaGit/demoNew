95420e9384cbe9b3bf8cc5c61c08348de1ba3abc
shweta.zagade@algorhythm.co.in <applifire@836552917ff5>
2016-06-03 13:25:18 +0000

    File deleting

--- testbp/src/main/java/com/app/server/businessservice/testbc/EmpQBzService.java
+++
@@ -1,8 +0,0 @@
-package com.app.server.businessservice.testbc;
-import com.app.shared.testbc.EmpQ;
-import java.util.List;
-
-public interface EmpQBzService {
-
-    public List<EmpQ> executeQueryEmpQ() throws Exception;
-}
